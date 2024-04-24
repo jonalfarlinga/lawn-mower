86dd91470238462f9b521478bf46fa25fa37a1e2
Denny Bucklin <dennis.bucklin@gmail.com>
2024-04-23 19:35:38 -0500

    delete dupes

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ imports os, shutils, sched, time
 #       for each item in list
 #           move item from sweep to dustbin
 '''
-
 # imports os, shutils, sched, time
-from shutil import move, Error
-from os import listdir, path, mkdir
+import shutil
+from os import listdir, path, mkdir, remove
 import schedule
 from time import sleep
 from datetime import datetime
@@ -42,7 +41,7 @@ def clean_up():
         print(">>created", DUSTBIN)
         BIN_CONTENTS = listdir(DUSTBIN)
 
-    print("__________________________________")
+    print("______________________________________")
     print(str(datetime.now()) + ": working...")
     #   for each path in sweeps
     for dir_path in SWEEPS:
@@ -51,29 +50,32 @@ def clean_up():
         try:
             contents = listdir(dir_path)
         except FileNotFoundError:
-            print("...... not found. Moving on...")
+            print("...... contents not found. Moving on...")
         #   for each item in list
         for item in contents:
             print("......", item)
             #   move item from sweep to dustbin
-            if item not in IGNORES and item not in BIN_CONTENTS:
-                try:
-                    dst = move(
-                        path.join(dir_path, item),
-                        DUSTBIN
-                    )
-                    BIN_CONTENTS.append(item)
-                    print("......... Moved to", dst)
-                except FileExistsError or Error:
-                    print(".........", item, "exists in", DUSTBIN)
-            else:
+            if item in IGNORES:
                 print("......... ignored")
+                continue
+
+            try:
+                dst = shutil.move(
+                    path.join(dir_path, item),
+                    DUSTBIN
+                )
+                BIN_CONTENTS.append(item)
+                print("......... Moved to", dst)
+            except shutil.Error:
+                print(".........", item, "exists in", DUSTBIN)
+                remove(path.join(dir_path, item))
+                print("......... deleted")
         if not contents:
             print("...... No items to move. Moving on...")
     print("Cleaning complete. Rescheduling...")
 
 
-#   schedule delay(10min)
+# schedule delay(10min)
 if __name__ == "__main__":
     print('Pulling rip cord...')
 
